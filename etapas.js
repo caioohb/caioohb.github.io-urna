c6ffe7796e08f92ef597fd7b78fee954a6715fce
Caio Henrique <92897314+caioohb@users.noreply.github.com>
2022-01-28 13:30:23 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Danzou Messias Bolsonaro',
+                partido: 'Anbu',
+                fotos: [
+                    { url: '38111.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Orochimaru Inácio da Silva Lula',
+                partido: 'Som',
+                fotos: [
+                    { url: '77222.png', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Sasori Marina Silva',
+                partido: 'Anbu',
+                vice: 'Sakura Danadinha',
+                fotos: [
+                    { url: '99.png', legenda: 'Prefeito' },
+                    { url: '99_2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Pain Roussef Estocador de Ventos',
+                partido: 'Som',
+                vice: 'Naruto UsuCrack',
+                fotos: [
+                    { url: '84.png', legenda: 'Prefeito' },
+                    { url: '84_2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
